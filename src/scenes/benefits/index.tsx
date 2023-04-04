fbd61ce24e2fbda4b583b8e568eab525776b0696
herom <jerome.manset22@gmail.com>
2023-04-05 00:15:44 +0800

    Create Htext shared component

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -1,5 +1,7 @@
+import Htext from "@/shared/Htext"
 import { SelectedPage } from "@/shared/types"
 import { HomeModernIcon, UserGroupIcon, AcademicCapIcon } from "@heroicons/react/24/solid"
+import { motion } from "framer-motion"
 
 type Props = {
     setSelectedPage: (value: SelectedPage) => void
@@ -7,7 +9,13 @@ type Props = {
 
 const Benefits = ({setSelectedPage}: Props) => {
   return (
-    <section id="benefits" className="mx-auto min-h-full">Benefits</section>
+    <section id="benefits" className="mx-auto min-h-full w-5/6 py-20">
+      <motion.div onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}>
+        <div>
+          <Htext>MORE THAN JUST GYM</Htext>
+        </div>
+      </motion.div>
+    </section>
   )
 }
 

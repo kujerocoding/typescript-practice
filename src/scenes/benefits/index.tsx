ad3e614583e9ccf1fc916e63b0533b5186bb8443
herom <jerome.manset22@gmail.com>
2023-04-05 01:04:30 +0800

    Add animation to the benefits elements

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -22,6 +22,13 @@ const benefits: Array<BenefitType> = [
   },
 ]
 
+const container = {
+  hidden: {},
+  visible: {
+    transition: { staggerChildren: 0.2 }
+  }
+}
+
 type Props = {
     setSelectedPage: (value: SelectedPage) => void
 }
@@ -30,12 +37,23 @@ const Benefits = ({setSelectedPage}: Props) => {
   return (
     <section id="benefits" className="mx-auto min-h-full w-5/6 py-20">
       <motion.div onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}>
-        <div className="md:my-5 md:w-3/5">
+        <motion.div 
+        initial="hidden" 
+        whileInView="visible" 
+        viewport={{once: true, amount: 0.5}} 
+        transition={{delay: 0.2, duration: 0.5}} 
+        variants={{hidden: {opacity: 0, x:-50}, visible: {opacity: 1, x:0}}}
+        className="md:my-5 md:w-3/5">
           <Htext>MORE THAN JUST GYM</Htext>
           <p className="my-5 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere deleniti omnis qui beatae quisquam temporibus quaerat dolores nobis itaque. Veniam!</p>
-        </div>
+        </motion.div>
         {/* BENEFITS */}
-        <div className="md:flex mt-5 items-center justify-between gap-8">
+        <motion.div className="md:flex mt-5 items-center justify-between gap-8" 
+        initial="hidden"
+        whileInView="visible"
+        viewport={{once: true, amount: 0.5}}
+        variants={container}
+        >
           {benefits.map((benefit: BenefitType) => (
             <Benefit key={benefit.title}
             icon={benefit.icon}
@@ -44,7 +62,7 @@ const Benefits = ({setSelectedPage}: Props) => {
             setSelectedPage={setSelectedPage}
             />
           ))}
-        </div>
+        </motion.div>
       </motion.div>
     </section>
   )

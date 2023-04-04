0df79d1ac83c5b83c9861ca0ac178672e7e571e4
herom <jerome.manset22@gmail.com>
2023-04-05 00:48:06 +0800

    Add benefits variables and declare object type in types.ts

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -1,7 +1,26 @@
 import Htext from "@/shared/Htext"
-import { SelectedPage } from "@/shared/types"
+import { BenefitType, SelectedPage } from "@/shared/types"
 import { HomeModernIcon, UserGroupIcon, AcademicCapIcon } from "@heroicons/react/24/solid"
 import { motion } from "framer-motion"
+import Benefit from "./Benefit"
+
+const benefits: Array<BenefitType> = [
+  {
+    icon: <HomeModernIcon className="h-6 w-6" />,
+    title: "State of the Art Facilities",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, minima?"
+  },
+  {
+    icon: <UserGroupIcon className="h-6 w-6" />,
+    title: "100's of Diverse Classes",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, minima?"
+  },
+  {
+    icon: <AcademicCapIcon className="h-6 w-6" />,
+    title: "Expert and Pro Trainers",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, minima?"
+  },
+]
 
 type Props = {
     setSelectedPage: (value: SelectedPage) => void
@@ -11,8 +30,20 @@ const Benefits = ({setSelectedPage}: Props) => {
   return (
     <section id="benefits" className="mx-auto min-h-full w-5/6 py-20">
       <motion.div onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}>
-        <div>
+        <div className="md:my-5 md:w-3/5">
           <Htext>MORE THAN JUST GYM</Htext>
+          <p className="my-5 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere deleniti omnis qui beatae quisquam temporibus quaerat dolores nobis itaque. Veniam!</p>
+        </div>
+        {/* BENEFITS */}
+        <div className="md:flex mt-5 items-center justify-between gap-8">
+          {benefits.map((benefit: BenefitType) => (
+            <Benefit key={benefit.title}
+            icon={benefit.icon}
+            title={benefit.title}
+            description={benefit.description}
+            setSelectedPage={setSelectedPage}
+            />
+          ))}
         </div>
       </motion.div>
     </section>
